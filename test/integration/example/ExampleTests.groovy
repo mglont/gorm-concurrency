439fc6c8c2b7a65dd6371d37d2fdbbf84fb143a8
Mihai Glonț <mglont@ebi.ac.uk>
2015-01-14 17:06:45 +0000

    committing latest changes to the tests.

--- test/integration/example/ExampleTests.groovy
+++ test/integration/example/ExampleTests.groovy
@@ -1,5 +1,7 @@
 package example
 
+import org.springframework.transaction.TransactionDefinition
+
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -12,24 +14,42 @@ import static org.junit.Assert.*
 
 @TestMixin(IntegrationTestMixin)
 class ExampleTests {
+    def dataSource
+    def sessionFactory
     @Before
     void setUp() {
-        User.withNewSession { session ->
-            User.withTransaction {
+/*        User.withNewSession { session ->
+            User.withTransaction([
+                    name: "setUp",
+                    propagationBehavior: TransactionDefinition.PROPAGATION_REQUIRED,
+                    isolationLevel: TransactionDefinition.ISOLATION_READ_COMMITTED
+            ]) {*/
                 10.times { i ->
                     def user = new User(username: "testUser$i", password: "secret")
                     assertTrue user.validate()
                     assertFalse user.hasErrors()
                     assertNotNull(user.save(validate: false))
                 }
-            }
-        }
+  /*          }
+        }*/
         assertEquals 10, User.count()
     }
 
     @After
     void tearDown() {
-        User.list().each { u -> u.delete() }
+/*        User.withSession { session ->
+            User.withTransaction([
+                    name: "tearDown",
+                    propagationBehavior: TransactionDefinition.PROPAGATION_REQUIRED,
+                    isolationLevel:
+                            TransactionDefinition.ISOLATION_READ_COMMITTED
+            ]) {*/
+                def users = User.list()
+                users.each { u ->
+                    u.delete()
+                }
+            /*}
+        }*/
         assertEquals 0, User.count()
     }
 
@@ -42,15 +62,15 @@ class ExampleTests {
             // creating new content in this session is all right
             def newUser = new User(username: "testUser${10 + i}", password: "letMeIn")
             assertTrue newUser.validate()
-            assertNotNull newUser.save()
+            assertNotNull newUser.save(validate: false, failOnError: true, flush: true)
             assertNotNull someUser
-            assertEquals 11 + i, User.count()
+            assertEquals(11 + i, User.count())
         }
     }
 
     @Test
     void testConcurrentSubmissions() {
-        final int SIZE = 2 // or any value less than 10
+        final int SIZE = 1 // or any value less than 10
         final CountDownLatch startLatch = new CountDownLatch(1)
         final CountDownLatch finishLatch = new CountDownLatch(SIZE)
         final AtomicReferenceArray<User> userRefs = new AtomicReferenceArray<>(SIZE)
@@ -60,25 +80,38 @@ class ExampleTests {
                 @Override
                 void run() {
                     startLatch.await()
+                    String un = "testUser${10 + i}"
                     try {
-                        User.withNewSession { session ->
-                            User.withTransaction {
-                                def someUser = User.findByUsername("testUser$i")
-                                // creating new content in this session is all right
-                                String un = "testUser${10+i}"
-                                assertNull(User.findByUsername(un))
-                                def newUser = new User(username: un, password: "letMeIn")
-                                assertTrue(newUser.validate())
-                                assertNotNull(newUser.save(validate: false))
-                                assertFalse(newUser.hasErrors())
-                                assertEquals(0, newUser.errors.allErrors.size())
-                                assertNotNull(newUser)
-                                userRefs.compareAndSet(i, null, newUser)
+                        User.withNewSession {
+                            User.withTransaction([
+                                    name: "worker$i".toString(),
+                                    propagationBehavior:
+                                            TransactionDefinition.PROPAGATION_REQUIRES_NEW,
+                                    isolationLevel:
+                                            TransactionDefinition.ISOLATION_READ_UNCOMMITTED
+                            ]) {
+                                try {
+                                    def someUser = User.findByUsername("testUser$i")
+                                    // creating new content in this session is all right
+                                    assertNull(User.findByUsername(un))
+                                    def newUser = new User(username: un, password: "letMeIn")
+                                    assertTrue(newUser.validate())
+                                    assertNotNull(newUser.save(validate: false, failOnError:
+                                            true, flush: true))
+                                    assertFalse(newUser.hasErrors())
+                                    assertEquals(0, newUser.errors.allErrors.size())
+                                    assertNotNull(newUser)
+                                    userRefs.compareAndSet(i, null, newUser)
 
-                                //TODO getting something inserted in another session is problematic
-                                assertNull someUser // SHOULD BE assertNotNull someUser
-                                assertEquals 1, User.count() // SHOULD BE assertEquals 10+i, User.count()
-
+                                    //TODO getting something inserted in another session is problematic
+                                    assertNotNull someUser
+                                    assertEquals 11 + i, User.count()
+                                } catch (Throwable e) {
+                                    println "you got error $e"
+                                    e.printStackTrace()
+                                    println "end of your stack trace"
+                                    fail("you lose!")
+                                }
                             }
                         }
                     } finally {
@@ -96,18 +129,26 @@ class ExampleTests {
         assertEquals 0, queuingJobs.size()
         assertTrue pool.isTerminated()
 
-        // see what we got
-        10.times { i ->
-            User expected = User.findByUsername("testUser$i")
-            assertNotNull expected
-        }
+        User.withNewSession { session ->
+            User.withTransaction([
+                    name: "mainThread-validation",
+                    propagationBehavior: TransactionDefinition.PROPAGATION_REQUIRES_NEW,
+                    isolationLevel: TransactionDefinition.ISOLATION_READ_UNCOMMITTED
+            ]) {
+                // see what we got
+                10.times { i ->
+                    User expected = User.findByUsername("testUser$i")
+                    assertNotNull expected
+                }
 
-        SIZE.times { i ->
-            User actual = userRefs.get(i)
-            User expected = User.findByUsername("testUser${10 + i}")
-            assertNotNull actual
-            assertNull expected //SHOULD BE assertNotNull expected
-            //assertEquals expected, actual
+                SIZE.times { i ->
+                    User actual = userRefs.get(i)
+                    User expected = User.findByUsername("testUser${10 + i}")
+                    assertNotNull actual
+                    assertNotNull expected
+                    assertEquals expected, actual
+                }
+            }
         }
     }
 }
